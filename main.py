49af3e959fcfc66d68c277e97e487b095ce5a7f1
Marianne Bezaire <mariannejcase@gmail.com>
2020-08-21 17:37:29 -0400

    Other parallel changes

--- main.py
+++ main.py
@@ -12,26 +12,28 @@ h('strdef RunName')
 h('PercentCellDeath = 0') # % of vulnerable interneurons to remove
 h('PercentAxonSprouting = 0') # % of extra connectivity allowed
 h('SimDuration = 100') # duration of simulation in milliseconds
-h('Scale = 1000') # How many real cells are represented by one
+h('Scale = 2000') # How many real cells are represented by one
                  # model cell (so larger numbers mean smaller models)
 #%%
 h.RunName = "Test"
-h.PercentCellDeath = 40
-h.PercentAxonSprouting = 40
-h.SimDuration = 1000
+h.PercentCellDeath = 50
+h.PercentAxonSprouting = 50
+h.SimDuration = 2000
+pc = h.ParallelContext()
 # check if dir exists
 # update RunName if necessary
-while (os.path.exists("results/"+h.RunName)):
-    st=h.RunName.find("_")
-    if (st>-1 and h.RunName[st+1:].isdigit()):
-        b='{:0'+str(len(h.RunName[st+1:]))+'d}'
-        h.RunName = h.RunName[:st+1] + b.format(int(h.RunName[st+1:])+1)
-    else:
-        h.RunName += '_00'
-
-#%%        
-os.mkdir("results/"+h.RunName)
-
+if (pc.id()==0):
+    while (os.path.exists("results/"+h.RunName)):
+        st=h.RunName.find("_")
+        if (st>-1 and h.RunName[st+1:].isdigit()):
+            b='{:0'+str(len(h.RunName[st+1:]))+'d}'
+            h.RunName = h.RunName[:st+1] + b.format(int(h.RunName[st+1:])+1)
+        else:
+            h.RunName += '_00'
+    
+    #%%        
+    os.mkdir("results/"+h.RunName)
+pc.barrier()
 h.load_file(1,"model-2.3.hoc")
 h.pc.gid_clear()
 
@@ -47,22 +49,38 @@ cell_ranges = {}
 with open('results/'+ROI+'/celltype.dat') as f:
     cell_reader = csv.DictReader(f,delimiter="\t")
     for cell in cell_reader:
-        cell_ranges[cell["celltype"]] = range(int(cell["rangeStart"]),int(cell["rangeEnd"])+1)
+        cell_ranges[cell["celltype"]] = [int(cell["rangeStart"]),int(cell["rangeEnd"])+1]
 
 
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
 
-mydata = np.loadtxt('results/'+ROI+'/spikeraster.dat')
-
-for key in cell_ranges:
-    tmpdata = mydata[mydata[:,1] in cell_ranges[key]]
-
-
 plt.figure()
-plt.scatter(mydata[:,0],mydata[:,1],s=.01)
+
+mydata = np.loadtxt('results/'+ROI+'/spikeraster.dat')
+#plt.scatter(mydata[:,0],mydata[:,1],s=.01)
+#plt.ylabel("Cell #")
+
+# Plotting interspersed cells in different colors by cell type:
+pt_colors=[np.array([1,1,0]),np.array([.2,.2,.2]),np.array([1,0,0]),np.array([0,1,0]),np.array([0,0,1])]
+#pt_colors=[np.array([1,1,0]),np.array([.2,0,.4]),np.array([1,.5,0]),np.array([.2,1,.9]),np.array([.8,.5,.8])]
+pt_c=0
+for key in cell_ranges:
+    tmpdata = mydata[(mydata[:,1]>=cell_ranges[key][0]) & (mydata[:,1]<cell_ranges[key][1])]
+    plotpos = (tmpdata[:,1] - cell_ranges[key][0])/(cell_ranges[key][1]-cell_ranges[key][0])*1000
+    if (len(plotpos)==1 and plotpos[0]==0):
+        plt.scatter(tmpdata[:,0],plotpos,s=20,c=pt_colors[pt_c].reshape(1,-1),label=key)
+    elif (len(plotpos)/len(mydata)<.5):
+        plt.scatter(tmpdata[:,0],plotpos,s=4,c=pt_colors[pt_c].reshape(1,-1),label=key)
+    else:
+        plt.scatter(tmpdata[:,0],plotpos,s=.05,c=pt_colors[pt_c].reshape(1,-1),label=key)
+    pt_c += 1
+    
+
 plt.title("{} with Cell Death: {}%, Sprouting: {}%".format(ROI, PercentCellDeath,PercentAxonSprouting))
 plt.xlabel("Time (ms)")
-plt.ylabel("Cell #")
+plt.ylabel("Cells (positioned)")
+plt.legend(loc="upper left",fontsize=8)
+#plt.xlim([200, 300])
 plt.show()
